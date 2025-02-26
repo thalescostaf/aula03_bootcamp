caeece2eac6b90b51f06b36f68e4c1609e12f708
Thales Fernandes <contato.thales@gmail.com>
2025-02-26 19:33:35 -0300

    Fluxo com Listas

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-print("Esse é o primeiro comando")
-print("Esse é o segundo comando")
-print("Esse é o terceiro comando")
-
 # O primeiro controle de fluxo é o IF
 # É o mais comum
 
-x = 15
+print('Primeiro exemplo')
+x = -5
 if x <= 2:
     print('É menor que zero')
 elif x <= 0:
     print('É menor que dois')
 else:
     print('É maior')
+
+
+print('===== Exercio para ver se é negativo')
+
+quantidade = -40
+preco = -3
+
+if quantidade > 0 and preco > 0:
+    print('Valores validos')
+else:
+    print('Valores negativos')
+
+
+print('===== Exemplo com FOR')
+
+for i in range (1,5):
+    print(i)
+
+
+
+print('===== Exercio listas')
+
+lista_de_alunos = ['Thales', 'Fabio', 'Luciano', 'Lais']
+
+for aluno in lista_de_alunos:
+    print(aluno)
+
+
