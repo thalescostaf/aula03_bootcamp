d0278248074fd3d5690147cc7c0fb2c25b84ab31
Thales Fernandes <contato.thales@gmail.com>
2025-02-26 19:12:37 -0300

    Fluxo com IF

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("Esse é o primeiro comando")
 print("Esse é o segundo comando")
 print("Esse é o terceiro comando")
+
+# O primeiro controle de fluxo é o IF
+# É o mais comum
+
+x = 15
+if x <= 2:
+    print('É menor que zero')
+elif x <= 0:
+    print('É menor que dois')
+else:
+    print('É maior')
